813d0cdaa76407670085cd67af25876f4ada3164
Elvis Xia <xux@microsoft.com>
2019-10-17 15:21:44 +0800

    Add project files.

---
+++ CompressionSample/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization.Formatters.Binary;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CompressionSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, RoutedEventArgs e)
+        {
+            Person person1 = new Person { FirstName = "Bill", LastName = "Smith" };
+            MyLibrary.Person person2 = new MyLibrary.Person { FirstName = "Bill", LastName = "Smith" };
+            MyLibrary.MyClass cls = new MyLibrary.MyClass();
+            MemoryStream compressedStream=cls.Compress(person2);
+            tbOriginal.Text = "Original Stream Length: " + cls.originalLength;
+            MemoryStream decompressedStream=Decompress(compressedStream);//length is still c2
+            tbDecompressed.Text = "Decompressed Stream Length: " + decompressedStream.Length;
+            
+        }
+        public MemoryStream Decompress(Stream compressedStream)
+        {
+            compressedStream.Position = 0;
+            MemoryStream decompressedStream = new MemoryStream();
+            
+            using (DeflateStream decompressor = new DeflateStream(compressedStream, CompressionMode.Decompress))
+            {
+                decompressor.CopyTo(decompressedStream);
+                decompressor.Flush();
+                //decompressor.Close();
+            }  // end using.
+
+            return decompressedStream;
+        }
+
+    }
+    [Serializable]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ MyLibrary/MyClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MyLibrary
+{
+    public class MyClass
+    {
+        public long originalLength;
+        public MemoryStream Compress(object p) {
+            MemoryStream originalStream = Serialize(p);//Length is c2
+            originalLength = originalStream.Length;
+            MemoryStream compressedStream = new MemoryStream();
+            DeflateStream compressor = new DeflateStream(compressedStream, CompressionMode.Compress);
+            originalStream.Position = 0;
+            originalStream.CopyTo(compressor);
+            compressor.Flush();
+            return compressedStream;
+
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        public MemoryStream Serialize(object p)
+        {
+
+            MemoryStream ms = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+            
+            formatter.Serialize(ms, p);
+
+            return ms;
+        }
+    }
+
+    [Serializable]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
+
